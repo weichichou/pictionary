b6f162c973f459f2287a17e826126d55bedcf950
weichichou <weichi.cc@gmail.com>
2020-04-23 22:26:49 +0200

    Show guessed answers on chat div

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,47 @@
 import React from "react";
 
 export default class Chat extends React.Component {
+  state = {
+    text: "",
+    //isSubmitted: false,
+    guessed: [],
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    /* this.setState({ isSubmitted: true }); */
+
+    this.setState({ guessed: [...this.state.guessed, this.state.text] });
+    this.setState({ text: "" });
+  };
+
   render() {
-    return <div>chat div</div>;
+    return (
+      <div>
+        <div>
+          {this.state.guessed.map((item) => (
+            <ul key={item}>{item}</ul>
+          ))}
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          {/* <lable>answer</lable> */}
+          <input
+            onChange={this.handleChange}
+            name="text"
+            value={this.state.text}
+            type="text"
+            placeholder="Your answer"
+            required
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
