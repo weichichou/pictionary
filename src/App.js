ed4c53168582d581ddeeea988438c4c6d4366363
weichichou <weichi.cc@gmail.com>
2020-04-23 21:57:17 +0200

    Add Chat div

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 //import logo from './logo.svg';
 import "./App.css";
 import Canvas from "./components/Canvas";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>app div</h1>
-        <Canvas />
+      <header>
+        <h1>pictionary</h1>
       </header>
+      <main className="main-div">
+        <Canvas />
+        <Chat />
+      </main>
     </div>
   );
 }
