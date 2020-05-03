c825d3db58d146a68b8f71341fa8e8e1e8a6f253
weichichou <weichi.cc@gmail.com>
2020-05-03 16:36:55 +0200

    Add socket.io whiteboard

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,35 +1,143 @@
 import React from "react";
-import CanvasDraw from "react-canvas-draw";
+
+//import CanvasDraw from "react-canvas-draw";
 
 export default class Canvas extends React.Component {
+  componentDidMount = () => {
+    //var socket = io();
+    var canvas = document.getElementsByClassName("whiteboard")[0];
+    //var colors = document.getElementsByClassName("color");
+    var context = canvas.getContext("2d");
+
+    var current = {
+      color: "black",
+    };
+    var drawing = false;
+
+    canvas.addEventListener("mousedown", onMouseDown, false);
+    canvas.addEventListener("mouseup", onMouseUp, false);
+    canvas.addEventListener("mouseout", onMouseUp, false);
+    canvas.addEventListener("mousemove", throttle(onMouseMove, 10), false);
+
+    //Touch support for mobile devices
+    canvas.addEventListener("touchstart", onMouseDown, false);
+    canvas.addEventListener("touchend", onMouseUp, false);
+    canvas.addEventListener("touchcancel", onMouseUp, false);
+    canvas.addEventListener("touchmove", throttle(onMouseMove, 10), false);
+
+    /* for (var i = 0; i < colors.length; i++) {
+      colors[i].addEventListener("click", onColorUpdate, false);
+    } */
+
+    //socket.on("drawing", onDrawingEvent);
+
+    // window.addEventListener("resize", onResize, false);
+    // onResize();
+
+    function getMousePos(canvas, evt) {
+      var rect = canvas.getBoundingClientRect();
+      return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top,
+      };
+    }
+
+    function drawLine(x0, y0, x1, y1, color, emit) {
+      context.beginPath();
+      context.moveTo(x0, y0);
+      context.lineTo(x1, y1);
+      context.strokeStyle = color;
+      context.lineWidth = 2;
+      context.stroke();
+      context.closePath();
+
+      if (!emit) {
+        return;
+      }
+      var w = canvas.width;
+      var h = canvas.height;
+
+      /*  socket.emit("drawing", {
+        x0: x0 / w,
+        y0: y0 / h,
+        x1: x1 / w,
+        y1: y1 / h,
+        color: color,
+      }); */
+    }
+
+    function onMouseDown(e) {
+      current.x = getMousePos(canvas, e).x;
+      current.y = getMousePos(canvas, e).y;
+
+      drawing = true;
+      //current.x = e.clientX || e.touches[0].clientX;
+      //current.y = e.clientY || e.touches[0].clientY;
+    }
+
+    function onMouseUp(e) {
+      if (!drawing) {
+        return;
+      }
+      drawing = false;
+      drawLine(
+        current.x,
+        current.y,
+        getMousePos(canvas, e).x,
+        getMousePos(canvas, e).y,
+        current.color,
+        true
+      );
+    }
+
+    function onMouseMove(e) {
+      console.log("client", e.clientX, e.clientY);
+      if (!drawing) {
+        return;
+      }
+      drawLine(
+        current.x,
+        current.y,
+        getMousePos(canvas, e).x,
+        getMousePos(canvas, e).y,
+        current.color,
+        true
+      );
+      current.x = getMousePos(canvas, e).x;
+      current.y = getMousePos(canvas, e).y;
+    }
+
+    function onColorUpdate(e) {
+      current.color = e.target.className.split(" ")[1];
+    }
+
+    // limit the number of events per second
+    function throttle(callback, delay) {
+      var previousCall = new Date().getTime();
+      return function () {
+        var time = new Date().getTime();
+
+        if (time - previousCall >= delay) {
+          previousCall = time;
+          callback.apply(null, arguments);
+        }
+      };
+    }
+
+    function onDrawingEvent(data) {
+      var w = canvas.width;
+      var h = canvas.height;
+      drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+    }
+
+    // make the canvas fill its parent
+    /*function onResize() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    } */
+  };
+
   render() {
-    return (
-      <div>
-        <button
-          //className={btnInBtnDiv}
-          onClick={() => {
-            this.myCanvas.clear();
-          }}
-        >
-          Clear
-        </button>
-        <button
-          //className={btnInBtnDiv}
-          onClick={() => {
-            this.myCanvas.undo();
-          }}
-        >
-          Undo
-        </button>
-        <CanvasDraw
-          className="canvas-drawing-div"
-          ref={(element) => (this.myCanvas = element)}
-          brushRadius={5}
-          lazyRadius={0}
-          canvasHeight="300px"
-          canvasWidth="500px"
-        />
-      </div>
-    );
+    return <canvas className="whiteboard" width="300" height="300"></canvas>;
   }
 }
